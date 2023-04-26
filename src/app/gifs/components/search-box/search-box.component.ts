870d4d8f52024a04df99759325b8c4a93e7c295f
Enrqiue Cabrera <ecabrera938gmail.com>
2023-04-25 23:34:41 -0600

    Cambio Readme

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gif-search-box',
+  template: `
+    <input type="text" class="form-control" placeholder="Buscar gifs..." />
+  `,
+})
+export class SearchBoxComponent {}
