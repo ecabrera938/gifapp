203f1eac97b99e258435493611cbfaaa51a89de1
Enrqiue Cabrera <ecabrera938gmail.com>
2023-04-27 23:00:03 -0600

    Fin de Seccion 7

--- src/app/gifs/services/gifs-service.service.ts
+++ src/app/gifs/services/gifs-service.service.ts
@@ -6,8 +6,7 @@ import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
   providedIn: 'root',
 })
 export class GifsService {
-
-  public gifList:Gif[] = [];
+  public gifList: Gif[] = [];
 
   private _tagHistory: string[] = [];
   private apiKey = 'WV7Ibdz9BtRlK1wc1LwcRZ0bKijutUlD';
@@ -21,30 +20,44 @@ export class GifsService {
     }
     this._tagHistory.unshift(tag);
     this._tagHistory = this._tagHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
-  constructor(private http: HttpClient) {}
-
+  constructor(private http: HttpClient) {
+    this.readLocalStorage()
+    
+  }
+  
   get tagHistory() {
     return [...this._tagHistory];
   }
-
+  
   searchTag(tag: string) {
     if (tag.length > 0) {
       // this._tagHistory.unshift(tag);
       this.organizeHistory(tag);
     }
     // console.log(this._tagHistory);
-
+    
     const params = new HttpParams()
-      .set('api_key', this.apiKey)
-      .set('limit', '12')
-      .set('q', tag)
-      .set('rating', 'g');
-
-    this.http.get<SearchResponse>(`${this.urlService}/search`, { params }).subscribe((resp) => {
-      this.gifList = resp.data
+    .set('api_key', this.apiKey)
+    .set('limit', '12')
+    .set('q', tag)
+    .set('rating', 'g');
+    
+    this.http
+    .get<SearchResponse>(`${this.urlService}/search`, { params })
+    .subscribe((resp) => {
+      this.gifList = resp.data;
       // console.log({gifs: this.gifList});
     });
+  }
+  private saveLocalStorage(): void {
+    localStorage.setItem('historial', JSON.stringify(this._tagHistory));
+  }
+  private readLocalStorage(): void {
+    if(!localStorage.getItem('historial'))return
+    this._tagHistory = JSON.parse(localStorage.getItem('historial')! )
+    this.searchTag(this._tagHistory.at(0)!)
   }
 }
